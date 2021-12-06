f5d0c5b5625fcf0dc0459db908ad1bc1fb635bb6
Hamdani Dinul Haq <dinulhaqhamdani41@gmail.com>
2021-12-06 20:46:05 +0700

    (Refactor) story

--- components/organisms/Story/index.tsx
+++ components/organisms/Story/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function Story() {
     return (
         <section className='story pt-50 pb-50'>
@@ -7,10 +9,10 @@ export default function Story() {
               className='col-lg-7 col-12 d-lg-flex d-none justify-content-lg-end pe-lg-60'
               data-aos='zoom-in'
             >
-              <img
+              <Image
                 src='/img/Header-9.png'
-                width='612'
-                height='452'
+                width={612}
+                height={452}
                 className='img-fluid'
                 alt=''
               />
