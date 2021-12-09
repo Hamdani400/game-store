285454d387b3c03d90fd6586e19f2af44f34563d
Hamdani Dinul Haq <dinulhaqhamdani41@gmail.com>
2021-12-09 15:21:20 +0700

    (Refactor) Sidebar member page

---
+++ components/organisms/SideBar/MenuItem.tsx
@@ -0,0 +1,34 @@
+import cx from 'classnames'
+import Image from 'next/image'
+
+interface MenuItemProps {
+    title: string;
+    active?: boolean;
+    icon: 'icon-menu-overview'
+    | 'icon-menu-messages'
+    | 'icon-menu-transaction'
+    | 'icon-menu-rewards'
+    | 'icon-menu-settings'
+    | 'icon-menu-logout'
+    | 'icon-menu-cards'
+}
+
+export default function MenuItem(props: Partial<MenuItemProps>) {
+    const { title, icon, active } = props
+    const classItem = cx({
+        item: true,
+        'mb-30': true,
+        active,
+    })
+
+    return (
+        <div className={classItem}>
+            <div className="me-3">
+                <Image src={`/icon/${icon}.svg`} width={25} height={25} />
+            </div>
+            <p className="item-title m-0">
+                <a href="" className="text-lg text-decoration-none">{title}</a>
+            </p>
+        </div>
+    )
+}
